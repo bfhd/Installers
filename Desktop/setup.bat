baab72a7f7b3be089d0c478b9012bd4bc49a0bf0
bfhd <boofhead@gmail.com>
2014-09-26 09:59:00 +1200

    OS version detection

--- Desktop/setup.bat
+++ Desktop/setup.bat
@@ -1,4 +1,42 @@
-start /wait msiexec /i "\\scribe-dev-01\Winscribe Client Software\Desktop\SSCERuntime_x64-ENU" /passive
-start /wait msiexec /i "C:\Users\adminlars\Desktop\Desktop\minimum\vc_runtimeMinimum_x64" /passive ADDEPLOY=1
-start /wait msiexec /i "C:\Users\adminlars\Desktop\Desktop\Additional\vc_runtimeAdditional_x64" /passive ADDEPLOY=1
-start /wait msiexec /i "\\scribe-dev-01\Winscribe Client Software\Desktop\x64\Winscribe Desktop x64.msi" /qb SERVERNAME="scribe-prod-01" RSDKCENTRALDBMS="LH-SQL-05\SCRIBE_PROD" RSDKCENTRALDBNAME="wsr" RSDKCENTRALDBLOGONUSER="winscribewsr" RSDKCENTRALDBLOGONPASSWORD="W1nscr1b3" RSDKLOCALCACHEPATH="C:\ProgramData\RSDK" RSDKTARGETDIR="C:\Program Files\RecognoscoSDK\"
+REM Find out OS version
+
+REM Windows XP
+ver | findstr /i "5\.1\."
+if %ERRORLEVEL% EQU 0 (
+goto ver_xp
+)
+
+REM Windows 7/8/8.1
+ver | findstr /i "6\.1\." > nul
+if %ERRORLEVEL% EQU 0 (
+goto ver_7
+)
+ver | findstr /i "6\.2\." > nul
+if %ERRORLEVEL% EQU 0 (
+goto ver_7
+)
+ver | findstr /i "6\.3\." > nul
+if %ERRORLEVEL% EQU 0 (
+goto ver_7
+)
+
+
+:ver_xp
+
+
+
+
+
+
+
+
+pushd "\\scribe-dev-01\Winscribe Client Software\Desktop"
+start /wait dotNetFx40_Full_x86_x64.exe /q /norestart
+start /wait msiexec /i SSCERuntime_x64-ENU /passive
+start /wait msiexec /i minimum\vc_runtimeMinimum_x64 /passive ADDEPLOY=1
+start /wait msiexec /i Additional\vc_runtimeAdditional_x64 /passive ADDEPLOY=1
+setup.exe /qb SERVERNAME="scribe-prod-01" RSDKCENTRALDBMS="LH-SQL-05\SCRIBE-PROD" RSDKCENTRALDBNAME="wsr" RSDKCENTRALDBLOGONUSER="winscribewsr" RSDKCENTRALDBLOGONPASSWORD="W1nscr1b3" RSDKLOCALCACHEPATH="C:\ProgramData\RSDK" RSDKTARGETDIR="C:\Program Files\RecognoscoSDK\"
+popd
+
+
+pause
