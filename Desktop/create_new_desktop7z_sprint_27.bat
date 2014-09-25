2d50568549aff588117c8c663ee5062a79e76eee
bfhd <boofhead@gmail.com>
2014-09-26 10:48:04 +1200

    create new 7z file for sprint 27

---
+++ Desktop/create_new_desktop7z_sprint_27.bat
@@ -0,0 +1,7 @@
+del /f desktop.7z
+7z a desktop27.7z .\vcredist\x64\*.msi
+7z a desktop27.7z .\vcredist\x86\*.msi
+7z a desktop27.7z .\SSCE\x64\*.msi
+7z a desktop27.7z .\SSCE\x86\*.msi
+7z a desktop27.7z .\dotnet4\dotNetFx40_Full_x86_x64.exe
+pause
