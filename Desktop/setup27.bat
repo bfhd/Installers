aca75b275324f5a668c4d73cb93b369cd34cf33d
bfhd <boofhead@gmail.com>
2014-10-14 09:40:25 +1300

    updated server address

--- Desktop/setup27.bat
+++ Desktop/setup27.bat
@@ -32,7 +32,8 @@ start /wait dotNetFx40_Full_x86_x64.exe /q /norestart
 start /wait msiexec /i SSCERuntime_x86-ENU /passive
 start /wait msiexec /i %~dp0x86\minimum\vc_runtimeMinimum_x86 /passive ADDEPLOY=1
 start /wait msiexec /i %~dp0x86\additional\vc_runtimeAdditional_x86 /passive ADDEPLOY=1
-%~dp0x86\setup.exe /qb SERVERNAME="119.161.41.81" RSDKCENTRALDBMS="119.161.41.81" RSDKCENTRALDBNAME="wsr" RSDKCENTRALDBLOGONUSER="wsrsqluser" RSDKCENTRALDBLOGONPASSWORD="W1nscr1b3" RSDKLOCALCACHEPATH="C:\Documents and Settings\All Users\Application Data\RSDK" RSDKTARGETDIR="C:\Program Files\RecognoscoSDK\"
+%~dp0x86\setup.exe /qb SERVERNAME="wscloudau2.winscribe.com" RSDKCENTRALDBMS="wscloudau2.winscribe.com" RSDKCENTRALDBNAME="wsr" RSDKCENTRALDBLOGONUSER="wsrsqluser" RSDKCENTRALDBLOGONPASSWORD="W1nscr1b3" RSDKLOCALCACHEPATH="C:\Documents and Settings\All Users\Application Data\RSDK" RSDKTARGETDIR="C:\Program Files\RecognoscoSDK\" RECOGNITIONADDRESS="net.tcp://wscloudau2.winscribe.com:8086/RcgServer"
+
 goto END
 
 REM Windows 7/8/8.1
@@ -47,7 +48,7 @@ start /wait dotNetFx40_Full_x86_x64.exe /q /norestart
 start /wait msiexec /i SSCERuntime_x64-ENU /passive
 start /wait msiexec /i %~dp0x64\minimum\vc_runtimeMinimum_x64 /passive ADDEPLOY=1
 start /wait msiexec /i %~dp0x64\additional\vc_runtimeAdditional_x64 /passive ADDEPLOY=1
-%~dp0x64\setup.exe /qb SERVERNAME="119.161.41.81" RSDKCENTRALDBMS="119.161.41.81" RSDKCENTRALDBNAME="wsr" RSDKCENTRALDBLOGONUSER="wsrsqluser" RSDKCENTRALDBLOGONPASSWORD="W1nscr1b3" RSDKLOCALCACHEPATH="C:\ProgramData\RSDK" RSDKTARGETDIR="C:\Program Files\RecognoscoSDK\"
+%~dp0x64\setup.exe /qb SERVERNAME="wscloudau2.winscribe.com" RSDKCENTRALDBMS="wscloudau2.winscribe.com" RSDKCENTRALDBNAME="wsr" RSDKCENTRALDBLOGONUSER="wsrsqluser" RSDKCENTRALDBLOGONPASSWORD="W1nscr1b3" RSDKLOCALCACHEPATH="C:\ProgramData\RSDK" RSDKTARGETDIR="C:\Program Files\RecognoscoSDK\" RECOGNITIONADDRESS="net.tcp://wscloudau2.winscribe.com:8086/RcgServer"
 goto END
 
 :32BIT
@@ -55,16 +56,24 @@ start /wait dotNetFx40_Full_x86_x64.exe /q /norestart
 start /wait msiexec /i SSCERuntime_x86-ENU /passive
 start /wait msiexec /i %~dp0x86\minimum\vc_runtimeMinimum_x86 /passive ADDEPLOY=1
 start /wait msiexec /i %~dp0x86\additional\vc_runtimeAdditional_x86 /passive ADDEPLOY=1
-%~dp0x86\setup.exe /qb SERVERNAME="119.161.41.81" RSDKCENTRALDBMS="119.161.41.81" RSDKCENTRALDBNAME="wsr" RSDKCENTRALDBLOGONUSER="wsrsqluser" RSDKCENTRALDBLOGONPASSWORD="W1nscr1b3" RSDKLOCALCACHEPATH="C:\ProgramData\RSDK" RSDKTARGETDIR="C:\Program Files\RecognoscoSDK\"
+%~dp0x86\setup.exe /qb SERVERNAME="wscloudau2.winscribe.com" RSDKCENTRALDBMS="wscloudau2.winscribe.com" RSDKCENTRALDBNAME="wsr" RSDKCENTRALDBLOGONUSER="wsrsqluser" RSDKCENTRALDBLOGONPASSWORD="W1nscr1b3" RSDKLOCALCACHEPATH="C:\ProgramData\RSDK" RSDKTARGETDIR="C:\Program Files\RecognoscoSDK\" RECOGNITIONADDRESS="net.tcp://wscloudau2.winscribe.com:8086/RcgServer"
 
 
 
 :END
 del /q %~dp0\*.*
 popd
+color a0
+echo ==================================================
+echo Installation complete!  Press any key to continue.
+echo ==================================================
+pause
 
 :ERROR
 REM del /q %~dp0\*.*
-echo Operating System not detected or not supported!
+color c0colo
+echo ======================================================
+echo Error: Operating System not detected or not supported!
+echo ======================================================
 pause
 popd
